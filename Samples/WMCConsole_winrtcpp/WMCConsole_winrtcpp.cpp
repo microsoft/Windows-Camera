912eaee4bd8d5138983de1c1a70b7792bb7ad74b
Sudhanshu Sohoni <susohoni@microsoft.com>
2019-05-21 16:37:47 -0700

    refactor code, added mock preview to drain samples from pipeline and allow 3A to converge

--- Samples/WMCConsole_winrtcpp/WMCConsole_winrtcpp.cpp
+++ Samples/WMCConsole_winrtcpp/WMCConsole_winrtcpp.cpp
@@ -31,115 +31,247 @@ using namespace Windows::Storage;
 using namespace Windows::Graphics::Imaging;
 using namespace Windows::Media::Core;
 
-// This is global because we need to hold the reference to sourcegroups. The sourceinfos are holding weak references to this.
-IVectorView<MediaFrameSourceGroup> sourceGroups;  
-
-//
-// Iterates through source groups and filter-out the frame sources which have IR, depth and other sources which we cannot consume in this app
-//
-IVector<MediaFrameSourceInfo> GetFilteredSourceGroupList()
+class CameraHelper
 {
-    auto filteredSourceInfos = single_threaded_vector<MediaFrameSourceInfo>();
-    sourceGroups = MediaFrameSourceGroup::FindAllAsync().get();
-    auto sourceGroupIter = sourceGroups.First();
-    while (sourceGroupIter.HasCurrent())
+
+    // We need to hold the reference to sourcegroups. The sourceinfos are holding weak references to this.
+    IVectorView<MediaFrameSourceGroup> m_SourceGroups;
+
+    // This can be used as a mock preview
+    MediaFrameReader m_PreviewFrameReader;
+
+    //
+    // Iterates through source groups and filter-out the frame sources which have IR, depth and other sources which we cannot consume in this app
+    //
+    IVector<MediaFrameSourceInfo> GetFilteredSourceGroupList()
     {
-        auto sourceInfos = sourceGroupIter.Current().SourceInfos();
-        auto sourceInfoIter = sourceInfos.First();
-        // Iterate through sources and filter-out the IR, depth and other sources which we cannot consume in this app
-        while (sourceInfoIter.HasCurrent())
+        auto filteredSourceInfos = single_threaded_vector<MediaFrameSourceInfo>();
+        m_SourceGroups = MediaFrameSourceGroup::FindAllAsync().get();
+        auto sourceGroupIter = m_SourceGroups.First();
+        while (sourceGroupIter.HasCurrent())
         {
-            if ((sourceInfoIter.Current().MediaStreamType() == MediaStreamType::Photo
-                || sourceInfoIter.Current().MediaStreamType() == MediaStreamType::VideoPreview
-                || sourceInfoIter.Current().MediaStreamType() == MediaStreamType::VideoRecord)
-                && sourceInfoIter.Current().SourceKind() == MediaFrameSourceKind::Color)
+            auto sourceInfos = sourceGroupIter.Current().SourceInfos();
+            auto sourceInfoIter = sourceInfos.First();
+            // Iterate through sources and filter-out the IR, depth and other sources which we cannot consume in this app
+            while (sourceInfoIter.HasCurrent())
             {
-                filteredSourceInfos.Append(sourceInfoIter.Current());
+                if ((sourceInfoIter.Current().MediaStreamType() == MediaStreamType::Photo
+                    || sourceInfoIter.Current().MediaStreamType() == MediaStreamType::VideoPreview
+                    || sourceInfoIter.Current().MediaStreamType() == MediaStreamType::VideoRecord)
+                    && sourceInfoIter.Current().SourceKind() == MediaFrameSourceKind::Color)
+                {
+                    filteredSourceInfos.Append(sourceInfoIter.Current());
+                }
+                sourceInfoIter.MoveNext();
             }
-            sourceInfoIter.MoveNext();
+            sourceGroupIter.MoveNext();
         }
-        sourceGroupIter.MoveNext();
+        return filteredSourceInfos;
     }
-    return filteredSourceInfos;
-}
 
-//
-// Prints information of all sources from the list in input argument 'filteredSources' and returns user selected index of the source to use
-//
-int GetSGSelection(IVector<MediaFrameSourceInfo> filteredSources)
-{
-    unsigned int idx = 0;
-    auto source = filteredSources.First();
-    while (source.HasCurrent())
+    //
+    // Prints information of all sources from the list in input argument 'filteredSources' and returns user selected index of the source to use
+    //
+    int GetSGSelection(IVector<MediaFrameSourceInfo> filteredSources)
     {
-        idx++;
-        auto currSource = source.Current();
+        unsigned int idx = 0;
+        auto source = filteredSources.First();
+        while (source.HasCurrent())
+        {
+            idx++;
+            auto currSource = source.Current();
 
-        // Add new entries to the following map initializations if new enum entries are added in the future
-        std::map<MediaStreamType,std::wstring> streamTypes = 
-        {
-            {MediaStreamType::VideoPreview, L"VideoPreview"},
-            {MediaStreamType::VideoRecord,  L"VideoRecord" },
-            {MediaStreamType::Audio,        L"Audio"       },
-            {MediaStreamType::Photo,        L"Photo"       }
-        };
+            // Add new entries to the following map initializations if new enum entries are added in the future
+            std::map<MediaStreamType, std::wstring> streamTypes =
+            {
+                {MediaStreamType::VideoPreview, L"VideoPreview"},
+                {MediaStreamType::VideoRecord,  L"VideoRecord" },
+                {MediaStreamType::Audio,        L"Audio"       },
+                {MediaStreamType::Photo,        L"Photo"       }
+            };
 
-        std::map<Panel, std::wstring>  panelTypes =
+            std::map<Panel, std::wstring>  panelTypes =
+            {
+                {Panel::Unknown, L"Unknown"},
+                {Panel::Front,   L"Front"  },
+                {Panel::Back,    L"Back"   },
+                {Panel::Top,     L"Top"    },
+                {Panel::Bottom,  L"Bottom" },
+                {Panel::Left,    L"Left"   },
+                {Panel::Right,   L"Right"  }
+            };
+            auto enclosureLocation = currSource.DeviceInformation().EnclosureLocation();
+            std::wstring panelLocation;
+            if (enclosureLocation)
+            {
+                panelLocation = L"\\" + panelTypes[enclosureLocation.Panel()];
+            }
+
+            std::wcout << idx << L":" << currSource.SourceGroup().DisplayName().c_str() << "-->" << currSource.DeviceInformation().Name().c_str() << panelLocation.c_str() << ":" << streamTypes[currSource.MediaStreamType()].c_str() << std::endl;
+            source.MoveNext();
+        }
+        do
         {
-            {Panel::Unknown, L"Unknown"},
-            {Panel::Front,   L"Front"  },
-            {Panel::Back,    L"Back"   },
-            {Panel::Top,     L"Top"    },
-            {Panel::Bottom,  L"Bottom" },
-            {Panel::Left,    L"Left"   },
-            {Panel::Right,   L"Right"  }
-        };
-        auto enclosureLocation = currSource.DeviceInformation().EnclosureLocation();
-        std::wstring panelLocation;
-        if (enclosureLocation)
+            std::wcout << L"Enter selection:";
+            std::cin >> idx;
+        } while ((idx > filteredSources.Size()) || (idx < 1));
+        return idx - 1; // Because selection idx is from 1 to N and indexes for from 0 to N-1
+    }
+
+    //
+    // Prints information of all available Mediatypes for the selected source and returns user selected index of the MediaType to use
+    //
+    int GetMediaTypeSelection(MediaFrameSourceInfo selectedSource)
+    {
+        auto mediaDescriptionIter = selectedSource.VideoProfileMediaDescription().First();
+        int idx = 0;
+
+        while (mediaDescriptionIter.HasCurrent())
         {
-            panelLocation = L"\\" + panelTypes[enclosureLocation.Panel()];
+
+            auto format = mediaDescriptionIter.Current();
+            int frameRateInt = (int)format.FrameRate();
+            int frameRateDec = ((int)(format.FrameRate() * 100) % 100);
+            idx++;
+            auto formatStr = std::to_wstring(idx) + L". " + std::wstring(format.Subtype().c_str()) + L": " +
+                std::to_wstring(format.Width()) + L"x" + std::to_wstring(format.Height()) + L" @ " + std::to_wstring(frameRateInt) + L"." + std::to_wstring(frameRateDec) + L" fps";
+            std::wcout << formatStr << std::endl;
+            mediaDescriptionIter.MoveNext();
+        }
+        int selection;
+        do
+        {
+            std::wcout << L"Enter selection:";
+            std::cin >> selection;
+        } while ((selection > idx) || (selection < 1));
+        return selection - 1; // Because selection idx is from 1 to N and indexes for from 0 to N-1
+    }
+
+    //
+    // Create a Frame reader for Mock preview and drain samples from preview/record pin for auto-exposure conververgence
+    //
+    void StartMockPreview(MediaCapture mediaCapture)
+    {
+        // start Preview to get the 3A running and wait for convergence.
+        MediaFrameSource previewframeSource(nullptr);
+        MediaStreamType streamTypelookup = MediaStreamType::VideoPreview;
+
+        // Try to find the suitable pin where 3A will be running.
+        // Start by looking for a preview pin , if not found look for record pin 
+        // However exit the loop when preview and record pins are not available as just running the photo pin cannot converge 3A
+        while ((previewframeSource == nullptr) && (streamTypelookup != MediaStreamType::Photo))
+        {
+            auto frameSourceIter = mediaCapture.FrameSources().First();
+            // If there is no preview pin, find a record pin
+            while (frameSourceIter.HasCurrent())
+            {
+                auto frameSource = frameSourceIter.Current().Value();
+                if (frameSource.Info().MediaStreamType() == streamTypelookup)
+                {
+                    previewframeSource = frameSource;
+                    break;
+                }
+                frameSourceIter.MoveNext();
+            }
+            streamTypelookup = (streamTypelookup == MediaStreamType::VideoPreview) ? MediaStreamType::VideoRecord : MediaStreamType::Photo;
         }
 
-        std::wcout << idx << L":" << currSource.SourceGroup().DisplayName().c_str() <<"-->" << currSource.DeviceInformation().Name().c_str() << panelLocation.c_str() << ":" << streamTypes[currSource.MediaStreamType()].c_str() << std::endl;
-        source.MoveNext();
+        m_PreviewFrameReader = mediaCapture.CreateFrameReaderAsync(previewframeSource).get();
+        m_PreviewFrameReader.AcquisitionMode(MediaFrameReaderAcquisitionMode::Realtime);
+        m_PreviewFrameReader.StartAsync().get();
+        std::cout << "\nDraining samples for one second from the pipeline to enable 3A convergence\n";
+        Sleep(1000);
     }
-    do
-    {
-        std::wcout << L"Enter selection:";
-        std::cin >> idx;
-    } while ((idx > filteredSources.Size()) || (idx < 1));
-    return idx-1; // Because selection idx is from 1 to N and indexes for from 0 to N-1
-}
 
-//
-// Prints information of all available Mediatypes for the selected source and returns user selected index of the MediaType to use
-//
-int GetMediaTypeSelection(MediaFrameSourceInfo selectedSource)
-{
-    auto mediaDescriptionIter = selectedSource.VideoProfileMediaDescription().First();
-    int idx = 0;
-
-    while (mediaDescriptionIter.HasCurrent())
+public:
+    CameraHelper() :m_PreviewFrameReader(nullptr), m_SourceGroups(nullptr)
     {
 
-        auto format = mediaDescriptionIter.Current();
-        int frameRateInt = (int)format.FrameRate();
-        int frameRateDec = ((int)(format.FrameRate() * 100) % 100);
-        idx++;
-        auto formatStr = std::to_wstring(idx) + L". " + std::wstring(format.Subtype().c_str()) + L": " +
-            std::to_wstring(format.Width()) + L"x" + std::to_wstring(format.Height()) + L" @ " + std::to_wstring(frameRateInt) + L"." + std::to_wstring(frameRateDec) + L" fps";
-        std::wcout << formatStr << std::endl;
-        mediaDescriptionIter.MoveNext();
     }
-    int selection;
-    do
+
+    //
+    // Initialized MediaCapture with appropriate settings, frame source and mediatype selection
+    //
+    MediaCapture InitCamera()
     {
-        std::wcout << L"Enter selection:";
-        std::cin >> selection;
-    } while ((selection > idx) || (selection < 1));
-    return selection-1; // Because selection idx is from 1 to N and indexes for from 0 to N-1
-}
+        MediaCapture mediaCapture;
+        auto settings = MediaCaptureInitializationSettings();
+        auto filteredGroups = GetFilteredSourceGroupList();
+
+        if (filteredGroups.Size() < 1)
+        {
+            std::cout << "Error No suitable capture sources found";
+            throw_hresult(MF_E_NO_CAPTURE_DEVICES_AVAILABLE);
+        }
+
+        auto selectedSGidx = GetSGSelection(filteredGroups);
+
+        auto selectedSrc = filteredGroups.GetAt(selectedSGidx);
+
+        if (selectedSrc == nullptr)
+        {
+            throw_hresult(MF_E_OUT_OF_RANGE);
+        }
+
+        settings.SourceGroup(selectedSrc.SourceGroup());
+
+        // If user explicitly selected a non-photo pin to take the photo
+        if (selectedSrc.MediaStreamType() != MediaStreamType::Photo)
+        {
+            settings.PhotoCaptureSource(PhotoCaptureSource::VideoPreview);
+        }
+        else
+        {
+            // We hope that auto will select the best photo pin options
+            settings.PhotoCaptureSource(PhotoCaptureSource::Auto);
+        }
+
+        settings.StreamingCaptureMode(StreamingCaptureMode::Video);
+
+        // Set format on the medicapture frame source
+        auto formatIdx = GetMediaTypeSelection(selectedSrc);
+
+        mediaCapture.InitializeAsync(settings).get();
+        auto frameSourceIter = mediaCapture.FrameSources().First();
+
+        auto frameSource = mediaCapture.FrameSources().Lookup(selectedSrc.Id());
+        frameSource.SetFormatAsync(frameSource.SupportedFormats().GetAt(formatIdx)).get();
+
+        if (mediaCapture.VideoDeviceController().ExposureControl().Supported())
+        {
+            std::cout << (mediaCapture.VideoDeviceController().ExposureControl().Auto() ? "Exposure Control AutoMode already Set" : "Exposure Control AutoMode not Set..setting it now to true");
+            mediaCapture.VideoDeviceController().ExposureControl().SetAutoAsync(true);
+        }
+        else if (mediaCapture.VideoDeviceController().Exposure().Capabilities().Supported())
+        {
+            if (mediaCapture.VideoDeviceController().Exposure().Capabilities().AutoModeSupported())
+            {
+                bool autoexp = false;
+                mediaCapture.VideoDeviceController().Exposure().TryGetAuto(autoexp);
+                std::cout << (autoexp ? "Exposure AutoMode already Set" : "Exposure AutoMode not Set..setting it now to true");
+                mediaCapture.VideoDeviceController().Exposure().TrySetAuto(true);
+            }
+            else
+            {
+                std::cout << "Exposure AutoMode not supported";
+            }
+        }
+
+        StartMockPreview(mediaCapture);
+
+        return mediaCapture;
+    }
+
+    void DeInitCamera(MediaCapture &mediaCapture)
+    {
+        m_SourceGroups = nullptr;
+        m_PreviewFrameReader.StopAsync().get();
+        m_PreviewFrameReader.Close();
+        m_PreviewFrameReader = nullptr;
+        mediaCapture.Close();
+        mediaCapture = nullptr;
+    }
+};
 
 //
 // Takes two photos and processes them using low light fusion API. Saves all photos to files
@@ -202,85 +334,6 @@ void TakePhotosAndProcess(MediaCapture mediaCapture, uint32_t photoIndex)
         throw(ex);
     }
 }
-
-//
-// Initialized MediaCapture with appropriate settings, frame source and mediatype selection
-//
-MediaCapture InitCamera()
-{
-    MediaCapture mediaCapture;
-    auto settings = MediaCaptureInitializationSettings();
-    auto filteredGroups = GetFilteredSourceGroupList();
-
-    if (filteredGroups.Size() < 1)
-    {
-        std::cout << "Error No suitable capture sources found";
-        throw_hresult(MF_E_NO_CAPTURE_DEVICES_AVAILABLE);
-    }
-
-    auto selectedSGidx = GetSGSelection(filteredGroups);
-
-    auto selectedSrc = filteredGroups.GetAt(selectedSGidx);
-
-    if (selectedSrc == nullptr)
-    {
-        throw_hresult(MF_E_OUT_OF_RANGE);
-    }
-
-    settings.SourceGroup(selectedSrc.SourceGroup());
-
-    // If user explicitly selected a non-photo pin to take the photo
-    if (selectedSrc.MediaStreamType() != MediaStreamType::Photo)
-    {
-        settings.PhotoCaptureSource(PhotoCaptureSource::VideoPreview);
-    }
-    else
-    {
-        // We hope that auto will select the best photo pin options
-        settings.PhotoCaptureSource(PhotoCaptureSource::Auto);
-    }
-
-    settings.StreamingCaptureMode(StreamingCaptureMode::Video);
-
-    // Set format on the medicapture frame source
-    auto formatIdx = GetMediaTypeSelection(selectedSrc);
-
-    mediaCapture.InitializeAsync(settings).get();
-    auto frameSourceIter = mediaCapture.FrameSources().First();
-    while (frameSourceIter.HasCurrent())
-    {
-
-        auto frameSource = frameSourceIter.Current().Value();
-        if (frameSource.Info().Id() == selectedSrc.Id())
-        {
-            frameSource.SetFormatAsync(frameSource.SupportedFormats().GetAt(formatIdx)).get();
-        }
-        frameSourceIter.MoveNext();
-    }
-
-    if (mediaCapture.VideoDeviceController().ExposureControl().Supported())
-    {
-        std::cout << (mediaCapture.VideoDeviceController().ExposureControl().Auto() ? "Exposure Control AutoMode already Set" : "Exposure Control AutoMode not Set..setting it now to true");
-        mediaCapture.VideoDeviceController().ExposureControl().SetAutoAsync(true);
-    }
-    else if (mediaCapture.VideoDeviceController().Exposure().Capabilities().Supported())
-    {
-        if (mediaCapture.VideoDeviceController().Exposure().Capabilities().AutoModeSupported())
-        {
-            bool autoexp = false;
-            mediaCapture.VideoDeviceController().Exposure().TryGetAuto(autoexp);
-            std::cout << (autoexp ? "Exposure AutoMode already Set" : "Exposure AutoMode not Set..setting it now to true");
-            mediaCapture.VideoDeviceController().Exposure().TrySetAuto(true);
-        }
-        else
-        {
-            std::cout << "Exposure AutoMode not supported";
-        }
-    }
-
-    return mediaCapture;
-}
-
 //
 // Main entry point of the application
 //
@@ -289,7 +342,8 @@ int wmain()
     std::cout << "Windows Media Capture in Win32 desktop Console app!";
     try
     {
-        auto mediaCapture = InitCamera();
+        CameraHelper cameraHelper;
+        auto mediaCapture = cameraHelper.InitCamera();
 
         // Main processing part of photos
         char key = 0;
@@ -309,6 +363,8 @@ int wmain()
                 continue;
             }
         }
+
+        cameraHelper.DeInitCamera(mediaCapture);
     }
     catch (hresult_error const&ex)
     {
