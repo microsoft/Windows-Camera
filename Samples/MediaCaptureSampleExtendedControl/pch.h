20ef7d6537a261ff4839deaf70e41893232d089d
Gerrit Swaneveld <55601905+gerritsMSFT@users.noreply.github.com>
2020-02-28 15:01:57 -0800

    Origin/user/gerrits/ks sample (#11)
    * MediaCapture Sample for using KS Extended Property Controls
    
    MediaCapture Sample for using KS Extended Property Controls. CustomExtendedProperty provides a sample class for managing and calling VideoDeviceController->GetDeviceProperty/SetDeviceProperty. The App calls into this class to manage a button and a slider, dependent on Device Driver support.
    
    * Update README.md
    
    * Create README.md
    
    * Added Copyright
    
    Added Copyright, removed false references
    
    * Update MainPage.xaml.cpp
    
    Remove typos
    
    * Fix Copyright
    
    Fix Copyright, remove unneeded lines

---
+++ Samples/MediaCaptureSampleExtendedControl/pch.h
@@ -0,0 +1,21 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+//
+// pch.h
+//
+
+#pragma once
+
+#include <collection.h>
+#include <ppltasks.h>
+
+#include <agile.h>
+#include "App.xaml.h"
